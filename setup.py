8b17ea3ecd7563c45906819eff91bbb1c41bf7ef
GitHub Action <action@github.com>
2024-03-09 19:58:58 +0000

    Bump version to 0.1.9

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ python_major_version = sys.version_info.major
 
 setup(
     name="create-pypi-cli",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     description=description,
     long_description=long_description,
